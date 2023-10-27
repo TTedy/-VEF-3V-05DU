22c90f984ed6392b8fc1d91cd6dedbbf5eca3f0e
þorvaldur <breki121212@gmail.com>
2023-10-27 11:22:06 +0000

    27.10.2023

--- Skilaverkefni/tskoli-project/src/app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <body className="bg-black">
-      <div id="nav" className="container mx-auto "> </div>
-      <div id="maincontent" className="container mx-auto"> </div>
-      <div id="footer" className="container mx-auto"> </div>
-    </body>
-  )
-}
