db1060aa9ec8fadb8699ae7d25e1b8ae6c702d84
þorvaldur <breki121212@gmail.com>
2023-11-15 14:59:05 +0000

    15.11.2023

--- Skilaverkefni/my-project/src/App.js
+++ Skilaverkefni/my-project/src/App.js
@@ -1,57 +1,37 @@
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  return (
-    
-    <body>
-      <nav class="bg-primary p-3">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="#" class="flex items-center">
-            <img src="my-project\src\img\parkl.png" alt="park"/>
-          </a>
-          <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-              </li>
-              <li>
-                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-              </li>
-              <li>
-                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-              </li>
-              <li>
-                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <main>
-        <img src={process.env.PUBLIC_URL + '/img/parklot.png'} alt="Park Lot r" />
-      </main>
-      <footer class="bg-primary grid grid-cols-3 p-3 justify-center items-center">
-        <div class="grid grid-cols-1">
-          <h1>Tækniskólinn parking lot</h1>
-          Front and behind of Tækniskólinn
-          <p>Háteigsvegur</p>
-          <p>Skólavörðuholt?</p>
-          <p>Hafnafjörður hallo from gitproblems</p>
-        </div>
-        <div class="grid grid-cols-1">
-          <h1>Phone</h1>
-          <p>+3546853923</p>
-          <p>Contact Us</p>
-        </div>
-        <div class="grid grid-cols-1">
-          <h1>Names</h1>
-          <p>Park&Co</p>
-          <p>park@park.is</p>
-          <p>-VED-3F-05DU</p>
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
-        </div>
-      </footer>
-    </body>
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Remove event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <div>
+      {isMobile ? (
+        
+        // Content for mobile
+        <h1>This is a mobile view</h1>
+      
+        ) : (
+
+        // Content for larger screens
+        <h1>This is a desktop view</h1>
+      
+      )}
+    </div>
   );
 }
 
