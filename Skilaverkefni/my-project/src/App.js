f622b3085d972dab04dfac2a7d0ceb2346d4ab3f
þorvaldur <breki121212@gmail.com>
2023-11-16 12:08:10 +0000

    16.11.2023

--- Skilaverkefni/my-project/src/App.js
+++ Skilaverkefni/my-project/src/App.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-streetview/dist/leaflet-streetview.js';
-import 'leaflet-streetview/dist/leaflet-streetview.css';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
 import './App.css';
+
+// ... (previous imports)
 
 function App() {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
@@ -22,30 +22,41 @@ function App() {
     };
   }, []);
 
-  useEffect(() => {
-    // Initialize Leaflet Street View once the component is mounted
-    const mapOptions = {
-      center: [37.7749, -122.4194], // Default center (San Francisco)
-      zoom: 12, // Default zoom level
-    };
 
-    const map = L.map('myMap', mapOptions);
+  const mainButtonStyleContainer = {
+    position: 'absolute',
+    width: '100%',
+    bottom: '20px',
+    display: 'flex',
+    justifyContent: 'center',
+  };
+  const buttonStyle = {
+    width: '50px',
+    height: '50px',
+    backgroundColor: 'blue', // Replace with your desired color or use the 'primary' class
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '50%',
+    position: 'absolute',
+    left: '45%',
+    zIndex: 1000,
+  };
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap contributors',
-    }).addTo(map);
-
-    // Initialize Leaflet Street View
-    const streetViewOptions = {
-      position: 'bottomright', // Change the position as needed
-    };
-    const streetView = L.control.streetview(streetViewOptions).addTo(map);
-
-    return () => {
-      // Clean up resources on component unmount
-      map.remove();
-    };
-  }, []); // Empty dependency array ensures this effect runs only once
+  const mainButtonStyle = {
+    width: '100px', // Adjust the width as needed
+    height: '40px', // Adjust the height as needed
+    backgroundColor: 'green', // Replace with your desired color
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '5px', // Adjust the border radius as needed
+    position: 'absolute',
+    bottom: '10px',
+    left: '50%',
+    transform: 'translateX(-50%)', // Center the button horizontally
+    zIndex: 1000,
+  };
 
   return (
     <div>
@@ -53,11 +64,68 @@ function App() {
         // Content for mobile
         <>
           <h1>This is a mobile view</h1>
-          <div id="myMap" style={{ height: '400px', width: '100%' }}></div>
+          <div style={{ height: '100vh', width: '100%', position: 'relative' }}>
+            <MapContainer
+              center={[64.09, -21.8652]} // Centered on Reykjavik
+              zoom={12}
+              style={{ height: '100%', width: '100%', position: 'absolute' }}
+              dragging={true} // Allow dragging
+            >
+              <TileLayer
+                url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='© OpenStreetMap contributors'
+              />
+
+              <div>
+                {[1, 2, 3].map((item, index) => (
+                  <div
+                    key={item}
+                    style={{
+                      ...buttonStyle,
+                      top: `${10 + index * 60}px`, // Adjust the vertical position
+                    }}
+                  >
+                    <a
+                      style={{
+                        color: 'white',
+                        textAlign: 'center',
+                        lineHeight: '50px',
+                        textDecoration: 'none', // Optional: Remove underline from anchor
+                      }}
+                      href="#"
+                    >
+                      Click me!
+                    </a>
+                  </div>
+                ))}
+              </div>
+
+            <div style={mainButtonStyleContainer}>
+              <div style={mainButtonStyle}>
+                <a
+                  style={{
+                    color: 'white',
+                    textAlign: 'center',
+                    textDecoration: 'none', // Optional: Remove underline from anchor
+                  }}
+                  href="#"
+                >
+                  Select
+                </a>
+              </div>
+            </div>
+
+
+
+
+            </MapContainer>
+          </div>
         </>
       ) : (
         // Content for larger screens
-        <h1>This is a desktop view</h1>
+        <div style={{ height: '80vh', width: '80%', margin: 'auto' }}>
+          <h1>This is a desktop view</h1>
+        </div>
       )}
     </div>
   );
