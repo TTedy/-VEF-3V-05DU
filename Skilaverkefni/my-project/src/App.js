0d6093b2d2d6633cc5228482768eb247f3ad845b
þorvaldur <breki121212@gmail.com>
2023-11-29 15:46:59 +0000

    29.11.2023

--- Skilaverkefni/my-project/src/App.js
+++ Skilaverkefni/my-project/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
 import './App.css';
 import parkstadi from './parkstadi.json'; // Import JSON file directly
@@ -8,6 +8,9 @@ function App() {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
   const [geojsonData, setGeojsonData] = useState(null);
   const [showDiv, setShowDiv] = useState(false);
+
+    // Define a state variable for the pin's coordinates
+    const [pinCoords, setPinCoords] = useState([64.09, -21.8652]); // Default to Reykjavik
 
   const toggleShowDiv = () => {
     setShowDiv(true);
@@ -22,7 +25,13 @@ function App() {
     setShowDiv(false);
   };
 
-  
+  const handleMarkerDrag = (e) => {
+    // Get the new position when the marker is dragged
+    const newPosition = e.target.getLatLng();
+    // Update the pinCoords state with the new position
+    setPinCoords([newPosition.lat, newPosition.lng]);
+  };
+
   useEffect(() => {
     // Add event listener for window resize
     window.addEventListener('resize', handleResize);
@@ -105,6 +114,12 @@ function App() {
               zoom={12}
               className="mapContainer"
               dragging={true} // Allow dragging
+              onmoveend={(e) => {
+                // Get the current center of the map
+                const center = e.target.getCenter();
+                // Update the pinCoords state with the new center
+                setPinCoords([center.lat, center.lng]);
+              }}
             >
               <TileLayer
                 url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -115,7 +130,12 @@ function App() {
               {geojsonData && <GeoJSON data={geojsonData} />}
 
               
-              {showDiv ? null : <div className="pin1"></div>}
+              {/* Replace the div with className "pin1" with a Marker component */}
+              <Marker position={pinCoords} draggable={true} onDragend={handleMarkerDrag}>
+                <Popup>
+                  <div className="pin1"></div>
+                </Popup>
+              </Marker>
 
               <div>
               {showDiv ? (
