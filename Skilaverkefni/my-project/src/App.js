9eba41758ea2535eac02ace1678dc79b4f064545
þorvaldur <breki121212@gmail.com>
2023-12-01 10:40:56 +0000

    1.12.2023

--- Skilaverkefni/my-project/src/App.js
+++ Skilaverkefni/my-project/src/App.js
@@ -4,17 +4,71 @@ import 'leaflet/dist/leaflet.css';
 import './App.css';
 import parkstadi from './parkstadi.json';
 
+function Map({pinCoords, center, zoom, markerPosition, showDiv, onMapMove, onMarkerDrag, onToggleShowDiv, onConfirmChoice, geojsonData }) {
+  return (
+    <MapContainer center={center} zoom={zoom} className="mapContainer" dragging={true} onmoveend={onMapMove}>
+      <TileLayer
+        url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='© OpenStreetMap contributors'
+      />
+
+      {geojsonData && <GeoJSON data={geojsonData} />}
+
+      {showDiv ? null : <div className="pin1"></div>}
+
+      <div>
+        {showDiv ? (
+          <div className="parkmenu container">
+            <div className="" id="time"><a href="#">time</a></div>
+            <div className="" id="date"><a href="#">date</a></div>
+            <div className="" id="cancel" onClick={() => onToggleShowDiv(false)}><a href="#">cancel</a></div>
+            <div className="" id="confirm" onClick={onConfirmChoice}><a href="#">confirm</a></div>
+          </div>
+        ) : (
+          <div className="button" id="park-button" onClick={() => onToggleShowDiv(true)}><a href="#">PARK</a></div>
+        )}
+      </div>
+
+      {markerPosition && (
+        <Marker position={pinCoords} draggable={true} onDrag={onMarkerDrag}>
+          {console.log(markerPosition)}
+          <Popup>A popup!</Popup>
+        </Marker>
+      )}
+    </MapContainer>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function App() {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
   const [geojsonData, setGeojsonData] = useState(null);
   const [showDiv, setShowDiv] = useState(false);
   const [pinCoords, setPinCoords] = useState([64.09, -21.8652]);
-  const [markerPosition, setMarkerPosition] = useState(pinCoords);
 
   const toggleShowDiv = () => setShowDiv(!showDiv);
 
   const handleMarkerDrag = (event) => {
-    setMarkerPosition(event.target.getLatLng());
+    setPinCoords(event.target.getLatLng());
   };
 
   const handleMapMove = (e) => {
@@ -27,13 +81,18 @@ function App() {
   const confirmChoice = () => setShowDiv(false);
 
   useEffect(() => {
+    const logMarkerPosition = setInterval(() => {
+      console.log('Marker Position:', pinCoords);
+    }, 1000); // Adjust the interval as needed (in milliseconds)
+
+    return () => clearInterval(logMarkerPosition);
+  }, [pinCoords]);
+
+
+  useEffect(() => {
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
-
-  useEffect(() => {
-    console.log('pincords', pinCoords);
-  }, [pinCoords]);
 
   useEffect(() => {
     setGeojsonData(parkstadi);
@@ -65,69 +124,33 @@ function App() {
         <>
           <h1>This is a mobile view</h1>
           <div className="mobile-view">
-            <MapContainer center={[64.09, -21.8652]} zoom={12} className="mapContainer" dragging={true}>
-              <TileLayer
-                url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='© OpenStreetMap contributors'
-              />
-
-              {geojsonData && <GeoJSON data={geojsonData} />}
-
-              {showDiv ? null : <div className="pin1"></div>}
-
-              <div>
-                {showDiv ? (
-                  <div className="parkmenu container">
-                    <div className="" id="time"><a href="#">time</a></div>
-                    <div className="" id="date"><a href="#">date</a></div>
-                    <div className="" id="cancel" onClick={() => setShowDiv(false)}><a href="#">cancel</a></div>
-                    <div className="" id="confirm" onClick={confirmChoice}><a href="#">confirm</a></div>
-                  </div>
-                ) : (
-                  <div className="button" id="park-button" onClick={toggleShowDiv}><a href="#">PARK</a></div>
-                )}
-              </div>
-            </MapContainer>
+            <Map
+              center={[64.09, -21.8652]}
+              zoom={12}
+              markerPosition={pinCoords}
+              showDiv={showDiv}
+              onMapMove={handleMapMove}
+              onMarkerDrag={handleMarkerDrag}
+              onToggleShowDiv={toggleShowDiv}
+              onConfirmChoice={confirmChoice}
+              geojsonData={geojsonData}
+            />
           </div>
         </>
       ) : (
         <div className="desktop-view">
           <h1>This is a desktop view</h1>
-          <MapContainer
+          <Map
             center={[64.09, -21.8652]}
             zoom={12}
-            className="mapContainer"
-            dragging={true}
-            onmoveend={(e) => {
-              const center = e.target.getCenter();
-              setPinCoords([center.lat, center.lng]);
-            }}
-          >
-            <TileLayer
-              url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution='© OpenStreetMap contributors'
-            />
-
-            {geojsonData && <GeoJSON data={geojsonData} />}
-
-            <Marker position={pinCoords} draggable={true} onDrag={handleMarkerDrag} >
-              <Popup>A popup!</Popup>
-            </Marker>
-
-            <div>
-              {showDiv ? (
-                <div className="parkmenu container">
-                  <h1>viltu leggja við?</h1>
-                  <div className="" id="time"><a href="#">time</a></div>
-                  <div className="" id="date"><a href="#">date</a></div>
-                  <div className="" id="cancel" onClick={() => setShowDiv(false)}><a href="#">cancel</a></div>
-                  <div className="" id="confirm" onClick={confirmChoice}><a href="#">confirm</a></div>
-                </div>
-              ) : (
-                <div className="button" id="park-button" onClick={toggleShowDiv}><a href="#">PARK</a></div>
-              )}
-            </div>
-          </MapContainer>
+            markerPosition={pinCoords}
+            showDiv={showDiv}
+            onMapMove={handleMapMove}
+            onMarkerDrag={handleMarkerDrag}
+            onToggleShowDiv={toggleShowDiv}
+            onConfirmChoice={confirmChoice}
+            geojsonData={geojsonData}
+          />
         </div>
       )}
     </div>
