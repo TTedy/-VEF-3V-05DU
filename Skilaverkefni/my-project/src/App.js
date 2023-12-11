e0ff9afb6cd60112fc3572bf19fef732b8dd3788
5Kall <vilhjalmurha21@tskoli.is>
2023-12-11 12:26:50 +0000

    ok

--- Skilaverkefni/my-project/src/App.js
+++ Skilaverkefni/my-project/src/App.js
@@ -1,153 +1,99 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, GeoJSON, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import parkstadi from './parkstadi.json';
-import { initializeApp } from 'firebase/app';  // Import initializeApp
+import firebase from 'firebase/app';
 import 'firebase/auth';
-import 'firebase/firestore';
-import db from './firebase-config';  // Make sure this import is correct
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAfdMNAyBga0KjMsJF_u_LH7ScRxf5H9pQ",
-  authDomain: "reactfirebase-40062.firebaseapp.com",
-  projectId: "reactfirebase-40062",
-  storageBucket: "reactfirebase-40062.appspot.com",
-  messagingSenderId: "66072772218",
-  appId: "1:66072772218:web:e78f07de16234fe19445c3"
-};
 
 
-// Initialize Firebase með gefnum stillingum
-firebase.initializeApp(firebaseConfig);
+import { firebaseConfig } from './firebase-config.js';
 
-// Skilgreina fall sem sýnir login-formið
-const Login = () => {
-  // Tveir stöðu breytur fyrir netfang og lykilorð
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  // Fall sem keyrir þegar ýtt er á "Login" hnappinn
-  const handleLogin = async () => {
-    try {
-      // Innskrá notanda með gefnum upplýsingum
-      const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password);
-      const user = userCredential.user;
-      console.log('Logged in user:', user);
-      // Hægt er að bæta við fleiri virkni, eins og að senda notanda á önnur síðu eftir vel heppnaða innskráningu
-    } catch (error) {
-      console.error('Error during login:', error.message);
-      // Meðhöndla innskráningarvillu, til dæmis með því að sýna villuskilaboð notanda
-    }
-  };
-
-  // Breyta sem stjórnar sýningu login-forms
-  const [showLogin, setShowLogin] = useState(false);
-
-  // Fall sem sýnir/felur login-formið
-  const toggleShowLogin = () => {
-    setShowLogin(!showLogin);
-  };
-
-  return (
-    <div>
-      {showLogin ? (
-        <div className="login-container">
-          <h2>Login</h2>
-          <div className="input-group">
-            <label htmlFor="username">Netfang:</label>
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Lykilorð:</label>
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          </div>
-          <button className="login-btn" onClick={handleLogin}>Login</button>
-          <button className="cancel-btn" onClick={() => setShowLogin(false)}>Hætta við</button>
-        </div>
-      ) : (
-        <div className="loginDesk" id="login-button" onClick={toggleShowLogin}>
-          <a href="#">INNSKRÁ</a>
-        </div>
-      )}
-    </div>
-  );
-};
-
-// Fall sem sýnir aðalútlitið
-function App() {
-  // Breyta sem stjórnar því hvort við erum á símasníði eða stöðvum útliti
+export default function App() {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
   const handleResize = () => setIsMobile(window.innerWidth <= 768);
   const [geojsonData, setGeojsonData] = useState(null);
   const [showDiv, setShowDiv] = useState(false);
-  const myElement = document.getElementById('marker');
 
-  // Fall sem keyrir þegar ýtt er á marki á kortinu
-  const handleMarkerClick = () => {
-    console.log('Merki ýtt á!');
-    // Bættu við eigin virkni hér
+  firebase.initializeApp(firebaseConfig);
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password);
+      const user = userCredential.user;
+      console.log('Logged in user:', user);
+    } catch (error) {
+      console.error('Error during login:', error.message);
+    }
   };
 
-  // Fall sem sýnir/felur valmynd um að parka
+  const [showLogin, setShowLogin] = useState(false);
+
+  const toggleShowLogin = () => {
+    setShowLogin(!showLogin);
+  };
+
+  /*
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      console.log('Clicked Coords:', clickedCoords);
+      // You can add any additional logic here
+    }, 1500);
+  
+    // Cleanup the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, [clickedCoords]); // Include clickedCoords in the dependency array
+  */
+
+
   const toggleShowDiv = () => setShowDiv(!showDiv);
 
-  // Fall sem felur valmyndina
+  
   const confirmChoice = () => {
     setShowDiv(false);
   };
+  
 
-  // Nota useEffect til að hlusta á stærð glugga
   useEffect(() => {
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  // Sækja GeoJSON gögn þegar komponenti er fyrst renderaður
   useEffect(() => {
     setGeojsonData(parkstadi);
   }, []);
 
-  // Sækja GeoJSON gögn frá vefþjóni
   useEffect(() => {
     const fetchGeoJSON = async () => {
       try {
         const response = await fetch("parkstadi.json");
 
         if (!response.ok) {
-          throw new Error(`HTTP villa! Stöða: ${response.status}`);
+          throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
         const data = await response.json();
         setGeojsonData(data);
-        console.log('GeoJSON gögn hlaðin:', data);
+        console.log('GeoJSON data loaded:', data);
       } catch (error) {
-        console.log('Villa við að sækja GeoJSON gögn:', error);
+        console.log('Error fetching GeoJSON data:', error);
       }
     };
 
     fetchGeoJSON();
   }, []);
 
-  // Hlusta á ýmis atburði á merki, til að bæta við eigin virkni
-  useEffect(() => {
-    if (myElement) {
-      myElement.addEventListener('click', handleMarkerClick);
-    }
-
-    return () => {
-      if (myElement) {
-        myElement.removeEventListener('click', handleMarkerClick);
-      }
-    };
-  }, [myElement]);
 
   return (
     <div>
       {isMobile ? (
         <>
-          <h1>Þetta er símasniðsútlit</h1>
+          <h1>This is a mobile view</h1>
           <div className="mobile-view">
-            {/* ... (þitt núverandi JSX kóði fyrir símasniðsútlitið) */}
+
             <MapContainer
               center={[64.09, -21.8652]}
               zoom={12}
@@ -155,24 +101,36 @@ function App() {
               onToggleShowDiv={toggleShowDiv}
               onConfirmChoice={confirmChoice}
               geojsonData={geojsonData}
-              style={{ height: '100vh' }}
             >
               <TileLayer
                 url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='© OpenStreetMap aðilar'
+                attribution='© OpenStreetMap contributors'
               />
-              {/* ... (annar kortahluti) */}
-              <Marker id="marker" onClick={handleMarkerClick}>
+              <marker>
                 {geojsonData && <GeoJSON data={geojsonData} />}
-                <Popup>
-                  {/* ... (Efni í Popup glugga) */}
-                </Popup>
-              </Marker>
-              {/* ... (þitt núverandi JSX kóði fyrir MapContainer) */}
+                <popup>
+                    popup
+                </popup>
+              </marker>
               <div>
                 {showDiv ? (
                   <div className="parkmenu container">
-                    {/* ... (þitt núverandi JSX kóði fyrir parkmenu á símasniði) */}
+                    <div className="" id="time">
+                      <a href="#">TIME</a>
+                    </div>
+                    <div className="" id="date">
+                      <a href="#">DATE</a>
+                    </div>
+                    <div
+                      className=""
+                      id="cancel"
+                      onClick={() => setShowDiv(false)}
+                    >
+                      <a href="#">CANCEL</a>
+                    </div>
+                    <div className="" id="confirm" onClick={confirmChoice}>
+                      <a href="#">CONFIRM</a>
+                    </div>
                   </div>
                 ) : (
                   <div
@@ -180,7 +138,7 @@ function App() {
                     id="park-button"
                     onClick={toggleShowDiv}
                   >
-                    <a href="#">PARKERA</a>
+                    <a href="#">PARK</a>
                   </div>
                 )}
               </div>
@@ -190,37 +148,24 @@ function App() {
       ) : (
         <div className='container'>
           <div className="desktop-view flex flex-row" id="map">
-            {/* ... (þitt núverandi JSX kóði fyrir stöðvutúlitið) */}
-            <MapContainer
-              center={[64.09, -21.8652]}
-              zoom={12}
-              className="mapContainer"
-            >
-              <TileLayer
-                url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='© OpenStreetMap aðilar'
-              />
-              {geojsonData && <GeoJSON data={geojsonData} />}
-            </MapContainer>
-            {/* ... (þitt núverandi JSX kóði fyrir stöðvutúlitið) */}
             <div className="">
               {showDiv ? (
                 <div className="parkmenuDesk container">
                   <div className="" id="time">
-                    <a href="#">TÍMI</a>
+                    <a href="#">TIME</a>
                   </div>
                   <div className="" id="date">
-                    <a href="#">DAGSETNING</a>
+                    <a href="#">DATE</a>
                   </div>
                   <div
                     className=""
                     id="cancel"
                     onClick={() => setShowDiv(false)}
                   >
-                    <a href="#">HÆTTA</a>
+                    <a href="#">CANCEL</a>
                   </div>
                   <div className="" id="confirm" onClick={confirmChoice}>
-                    <a href="#">STAÐFESTA</a>
+                    <a href="#">CONFIRM</a>
                   </div>
                 </div>
               ) : (
@@ -229,14 +174,39 @@ function App() {
                   id="park-button"
                   onClick={toggleShowDiv}
                 >
-                  <a href="#">PARKERA</a>
+                  <a href="#">PARK</a>
                 </div>
               )}
               <div>
-                <Login />
+                {showLogin ? (
+                  <div class="login-container">
+                    <h2>Login</h2>
+                  
+                    <div class="input-group">
+                      <label for="username">Username:</label>
+                      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                  
+                    <div class="input-group">
+                      <label for="password">Password:</label>
+                      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                  
+                    <button class="login-btn" onclick="handleLogin()">Login</button>
+                    <button class="cancel-btn" onClick={() => setShowLogin(false)}>Cancel</button>
+                  </div>
+                ) : (
+                  <div className="loginDesk" 
+                  id="login-button" 
+                  onClick={toggleShowLogin}
+                  >
+                    <a href="#">LOGIN</a>
+                  </div>
+                )}
               </div>
             </div>
-            <div>
+              
+            <div>    
               <MapContainer
                 center={[64.09, -21.8652]}
                 zoom={12}
@@ -244,16 +214,20 @@ function App() {
               >
                 <TileLayer
                   url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution='© OpenStreetMap aðilar'
+                  attribution='© OpenStreetMap contributors'
                 />
+
                 {geojsonData && <GeoJSON data={geojsonData} />}
+
+
               </MapContainer>
             </div>
+
           </div>
         </div>
-      )}
+      )};
+
     </div>
   );
 }
-
-export default App;
+};
